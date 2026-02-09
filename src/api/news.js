5b9999fb26dd154515aed61ba0ba01cadee06c0b
zsc <160631139@qq.com>
2026-02-09 17:06:06 +0800

    feat:收藏页面的完成

--- src/api/news.js
+++ src/api/news.js
@@ -1,13 +1,26 @@
 import request from '@utils/request'//导入axios
 //新闻分类
 export function getCategoryList(){
-    return request.get('/news/category/list')//与基础路径产生关联
+    return request.get(`/news/category/list`)//与基础路径产生关联
 }
 //新闻列表
 export function getNewsList(params){
-    return request.get('/news/list',params)
+    return request.get(`/news/list`,params)
 }//params是传给后端的查询参数，将参数拼接到查询字符串上
 //新闻详情
 export function getNewsDetail(id){
-    return request.get('news/detail/${id}')
+    return request.get(`news/detail/${id}`)
+}
+//添加收藏
+export function addFavorite(id) {
+  return request.post(`/favorite/add/${id}`)
+}
+// 取消收藏
+export function cancelFavorite(id) {
+  return request.delete(`/favorite/cancel/${id}`)
+}
+//收藏列表
+export function getFavoriteList()
+{
+    return request.post(`/favorite/list`)
 }
--- src/components/News.vue
+++ src/components/News.vue
@@ -3,7 +3,21 @@
       <el-card 
       class="news-card"
       @click="goDetail(news.id)">
-      <h3>{{news.title}}</h3>
+      <div class="heaer">
+        <h3>{{news.title}}</h3>
+         <!-- 用于当点击的收藏按钮的时候不会直接进入详情
+         内容通过布尔值判断是否收藏过
+         -->
+        <el-button
+         size="small"
+         type="warning"
+         @click.stop="onToggleFavorite"
+         >
+         {{isFavorite?'取消收藏':'收藏'}}
+        </el-button>
+
+      </div>
+      
        <p>{{ news.summary }}</p>
       <span>{{ news.publishTime }}</span>
       </el-card>
@@ -11,16 +25,35 @@
 </template>
 <script setup>
 import{useRouter}  from 'vue-router'
+import {ref} from 'vue'
+import{addFavorite,cancelFavorite}from '@/api/news'
+import {ElMessage} from 'element-plus'
+
 const props=defineProps({//调用vue内置宏
     news:{//定义一个prop名为news
         type:Object,//传入的类型必须是js对象
         required:true//强制必须传递
     }
+  
 })
+const isFavorite=ref(false)
 const router=useRouter()
 const goDetail=()=>{
     router.push('/newsDetail/${props.news.id}')
 }
+const onToggleFavorite=async()=>{//点击按钮时要判断是否收藏过
+  if(isFavorite.value===true)
+  {
+    await cancelFavorite(props.news.id)
+    ElMessage.success('已取消收藏')
+  }
+  else{
+    await addFavorite(props.news.id)
+    ElMessage.success('已收藏')
+  }
+
+}
+
 
 </script>
 <style scoped>
@@ -42,6 +75,11 @@ const goDetail=()=>{
 .time {
   color: #999;
   font-size: 12px;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
     
 </style>
